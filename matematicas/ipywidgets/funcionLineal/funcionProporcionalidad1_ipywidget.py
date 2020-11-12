95a2c171289481b798aab18e02c19944b4838a3b
antoniorv <antonio.raju@gmail.com>
2020-11-12 11:45:47 +0100

    función lineal

---
+++ matematicas/ipywidgets/funcionLineal/funcionProporcionalidad1_ipywidget.py
@@ -0,0 +1,15 @@
+import ipywidgets
+from ipywidgets import interact
+import matplotlib.pyplot as plt
+import numpy as np
+
+def parabola(m = 1):
+    x = np.linspace(-10, 10, 200)
+    y = np.linspace(-10, 10, 200)
+    plt.axis([-10, 10, 0, 10])
+    #plt.grid(True)
+    plt.axvline(linewidth=3, color = 'r')
+    plt.axhline(linewidth=2, color = 'r')
+    plt.plot(y, (m*x))
+
+interact(parabola, m = (-3, 3))
